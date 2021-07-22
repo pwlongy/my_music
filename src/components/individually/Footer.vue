4d60f2883531a504b182894691908c6bc56726c4
penglywei <2405267405@qq.com>
2021-07-22 11:04:58 +0800

    首页footer基本样式完成

--- src/components/individually/Footer.vue
+++ src/components/individually/Footer.vue
@@ -8,15 +8,31 @@
     </div>
     <!-- 进度条 -->
     <div class="block">
+      <span>02:44</span>
       <el-slider 
       v-model="value"
       :show-tooltip= "false"
       >
       </el-slider>
+      <span>03:54</span>
     </div>
 
     <!-- 其他 -->
-    <div>
+    <div class="other">
+      <i class="iconfont icon-shengyin"></i>
+      <el-slider 
+        v-model="value1"
+        :show-tooltip= "false"
+        >
+            </el-slider>
+      <i class="iconfont icon-shunxubofang"></i>
+      <div>
+        <span>标准</span>
+        <i class="iconfont icon-xuanxiang"></i>
+        <span>词</span>
+      </div>
+      
+      <i class="iconfont icon-gedan"></i>
 
     </div>
   </div>  
@@ -32,7 +48,10 @@ export default {
   },
   data() {
     return {
-      value: 100
+      // 进度条
+      value: 100,
+      // 音量
+      value1: 50
     }
   }
 }
@@ -44,7 +63,7 @@ export default {
     display: flex;
     .play{
       height: 100%;
-      width: 250px;
+      width: 240px;
       display: flex;
       align-items: center;
       padding: 0 20px;
@@ -58,6 +77,7 @@ export default {
         line-height: 40px;
         color: #fff;
         margin: 0 10px ;
+        cursor: pointer;
       }
       span:nth-child(2){
         width: 45px;
@@ -68,14 +88,57 @@ export default {
 
     // 输入框
     .block{
-      width: 1120px;
-      padding-top: 10px;
-      .el-silder{
-        .el-slider__bar{
-          background-color: chocolate !important;
-        }
+      width: 1230px;
+      display: flex;
+      .el-slider{
+        width: 1120px;
+        padding-top: 13px;
       }
-     
+      span{
+        margin: 0 15px;
+      }
+    }
+
+    // 其他
+    .other{
+      margin-left: 20px;
+      display: flex;
+      .el-slider{
+        width: 130px;
+        padding-top: 13px;
+      }
+      .icon-shengyin{
+        font-size: 20px;
+        font-weight: 800;
+        margin-right: 10px;
+      }
+      .icon-shunxubofang{
+        font-size: 18px;
+        color: #000;
+        margin-left: 22px;
+      }
+      .icon-gedan{
+        font-size: 18px;
+        color: #000;
+        font-weight: 600; 
+      }
+      .icon-xuanxiang{
+        font-size: 18px;
+        font-weight: 600;
+      }
+      .iconfont{
+        cursor: pointer;
+      }
+      span{
+        margin: 0 22px;
+        border: 1px solid #999;
+        padding: 0 4px;
+        height: 25px;
+        line-height: 25px;
+        cursor: pointer;
+        
+      }
+      
     }
   }
 </style>
