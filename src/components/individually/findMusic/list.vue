484ca8ab7a54c0fe46fe9df03ddce77b1f803e8f
penglywei <2405267405@qq.com>
2021-07-25 12:36:22 +0800

    排行榜基本样式完成

--- src/components/individually/findMusic/list.vue
+++ src/components/individually/findMusic/list.vue
@@ -1,6 +1,302 @@
 <template>
   <div class="list">
-    <h1>4</h1>
+    <div class="title">
+      <h1>官方榜</h1>
+    </div>
+
+    <div class="bootom">
+      <div class="listItem">
+        <ul>
+          <li>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          
+          <li>
+            <span>查看全部<i class="el-icon-arrow-right"></i></span>
+          </li>
+        </ul>
+      </div>
+      <div class="listItem">
+        <ul>
+          <li>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          
+          <li>
+            <span>查看全部<i class="el-icon-arrow-right"></i></span>
+          </li>
+        </ul>
+      </div>
+      <div class="listItem">
+        <ul>
+          <li>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          
+          <li>
+            <span>查看全部<i class="el-icon-arrow-right"></i></span>
+          </li>
+        </ul>
+      </div>
+      <div class="listItem">
+        <ul>
+          <li>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          
+          <li>
+            <span>查看全部<i class="el-icon-arrow-right"></i></span>
+          </li>
+        </ul>
+      </div>
+      <div class="listItem">
+        <ul>
+          <li>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          <li>
+            <h6>1</h6>
+            <span>687%</span>
+            <u>还是劈开</u>
+            <p>张叶蕾/全体诺克萨斯国战士</p>
+          </li>
+          
+          <li>
+            <span>查看全部<i class="el-icon-arrow-right"></i></span>
+          </li>
+        </ul>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -11,5 +307,81 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .list{
+    .title{
+      border-bottom: 1px solid #e1e1e2;
+      padding-top: 40px;
+      padding-bottom: 12px;
+      h1{
+        font-size: 22px;
+        font-weight: 400;
+      }
+    }
 
+    .bootom{
+      padding-top: 22px;
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-column-gap: 14px;
+      .listItem{
+        width: 416px;
+        height: 465px;
+        background: salmon;
+        margin-bottom: 20px;
+        border: 1px solid #ededed;
+        ul{
+          li:nth-child(1){
+            height: 114px;
+            background: sandybrown;
+          }
+          li{
+            height: 37px;
+            line-height: 37px;
+            background: lightgoldenrodyellow;
+            padding-left: 22px;
+            padding-right: 14px;
+            h6{
+              font-size: 16px;
+              color: #666666;
+              float: left;
+              margin-right: 10px;
+            }
+            span{
+              display: block;
+              float: left;
+              width: 38px;
+              text-align: left;
+              font-size: 12px;
+              color: #888888;
+            }
+            u{
+              font-size: 14px;
+              color: #000;
+              font-weight: 600;
+            }
+            p{
+              float: right;
+              width: 102px;
+              text-align: right;
+              font-size: 14px;
+              color: #888888;
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+            }
+          }
+          li:last-child{
+            height: 53px;
+            background: lightgreen;
+            span{
+              line-height: 53px;
+              cursor: pointer;
+              width: 80px;
+              float: right;
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
